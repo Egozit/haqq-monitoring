c1ff888048350d46a5751406acb66b54046288b7
NktSH <49846495+NktSH@users.noreply.github.com>
2022-09-18 19:55:14 +0300

    Delete variables.sh

--- variables.sh
+++
@@ -1,4 +0,0 @@
-#haqq monitoring variables template 
-COS_BIN_NAME=insert_path_to_haqq_binary # example: /root/go/bin/haqqd or /home/user/go/bin/haqqd
-COS_PORT_RPC=insert_port_here              # default: 12657
-COS_VALOPER=insert_valoper_address         # example: haqqvaloper1234545636767376535673
